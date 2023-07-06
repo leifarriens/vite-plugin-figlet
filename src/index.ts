f66ceae7fbbfd5b892e01ba936251e0fedd2a6eb
Leif Arriens <leif.arriens@gmail.com>
2023-07-06 23:13:54 +0200

    feat: add vite-plugin-figlet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function add(...args: number[]): number {
-  return args.reduce((curr, acc) => curr + acc, 0);
+import figlet, { type Options } from 'figlet';
+import { type PluginOption } from 'vite';
+
+export default function (txt: string, figletOptions?: Options): PluginOption {
+  return {
+    name: 'vite-plugin-figlet',
+    apply: 'serve',
+    configureServer() {
+      figlet(txt, figletOptions, (err, data) => {
+        if (err) return;
+        console.log(data);
+      });
+    },
+  };
 }
---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import type { Options } from 'tsup';
+
+export const tsup: Options = {
+  clean: true,
+  dts: true,
+  target: 'node14',
+  splitting: true,
+  format: ['esm', 'cjs'],
+  external: ['vite'],
+};
